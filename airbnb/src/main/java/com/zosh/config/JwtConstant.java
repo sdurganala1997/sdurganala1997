6d00ae0d8155e7299c6c56bf615076d47612ec03
Saideep Durganala <sdeep3015@gmail.com>
2024-08-04 18:32:00 -0400

    Security policy created

---
+++ airbnb/src/main/java/com/zosh/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.zosh.config;
+
+public class JwtConstant {
+	
+	public static final String SECRET_KEY="gdbvchberohvbfgrfvyurewfbyugbyuovwurthnyugfvhewtginerhbtbrevbtbuyrtbvyrbt";
+	public static final String JWT_HEADER="Authorization";
+
+}
